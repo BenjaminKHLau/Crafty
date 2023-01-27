849560555b2f3a74d97edd51f74119ab72375842
Benjamin Lau <benjaminkhlau@gmail.com>
2023-01-26 21:11:11 -0600

    styled edit and delete buttons for merch reviews on profile page

--- react-app/src/components/merch/userMRs.js
+++ react-app/src/components/merch/userMRs.js
@@ -2,25 +2,20 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import MerchRevCard from "./merchRevCard";
-import "./userMerchRev.css"
+import "./userMerchRev.css";
 
-function UserMerchReviews({user, reviews}){
-    // console.log(reviews)
-    
+function UserMerchReviews({ user, reviews }) {
+	// console.log(reviews)
 
-    return(
-        <div className="merch-rev-user">
-            {reviews.map(review => (
-                
-                    <div>
-
-                    <MerchRevCard user={user} review={review} />
-                    </div>
-
-
-            ))}
-        </div>
-    )
+	return (
+		<div className="merch-rev-user">
+			{reviews.map((review) => (
+				<div>
+					<MerchRevCard user={user} review={review} />
+				</div>
+			))}
+		</div>
+	);
 }
 
-export default UserMerchReviews
+export default UserMerchReviews;
