28e1ae0526e22aced69cb66fe9ba9f9572c453b9
Benjamin Lau <benjaminkhlau@gmail.com>
2023-08-04 14:24:28 -0500

    prettier format for merchCard

--- react-app/src/components/merch/merchCard.js
+++ react-app/src/components/merch/merchCard.js
@@ -1,24 +1,26 @@
+import sorrykiwi2 from "../pictures/sorrykiwi2.png";
 
-import sorrykiwi2 from "../pictures/sorrykiwi2.png"
+function MerchCard({ merch }) {
+  if (merch.name.length > 20) merch.name = merch.name.slice(0, 20) + "...";
 
-function MerchCard({merch}){
-
-    if (merch.name.length > 20) merch.name = merch.name.slice(0, 20) + "..."
-
-    return (
-        <div className="merch-card">
-            <div className="merch-home-image-container">
-            <img src={merch.merch_image_url} alt="merch pic" className="merch-home-image" onError={e => {e.target.src = sorrykiwi2}}/>
-            <div className="merch-card-info">
-            {merch.name}
-            </div>
-            {/* <div className="edit-delete" onClick={(e) => deleteMerch(e)}>
+  return (
+    <div className="merch-card">
+      <div className="merch-home-image-container">
+        <img
+          src={merch.merch_image_url}
+          alt="merch pic"
+          className="merch-home-image"
+          onError={(e) => {
+            e.target.src = sorrykiwi2;
+          }}
+        />
+        <div className="merch-card-info">{merch.name}</div>
+        {/* <div className="edit-delete" onClick={(e) => deleteMerch(e)}>
 							Delete Item
 						</div> */}
-            </div>
-
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default MerchCard;
