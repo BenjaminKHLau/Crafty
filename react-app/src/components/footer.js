3741326dd19f35182e520f1efeeee020f3ace52d
Benjamin Lau <benjaminkhlau@gmail.com>
2023-08-04 14:23:59 -0500

    prettier format for footer

--- react-app/src/components/footer.js
+++ react-app/src/components/footer.js
@@ -1,40 +1,42 @@
 import "./footer.css";
-import ben from "./pictures/ben.jpg"
-import linkedin from "./pictures/linkedin.png"
+import ben from "./pictures/ben.jpg";
+import linkedin from "./pictures/linkedin.png";
 
 function Footer() {
-	return (
-		<footer>
-			<div className="Footer-container">
-				<div className="devs">
-					<div className="Meet">Meet The Developer!</div>
-					<div className="Footer">
-						<div className="Footer-inner">
-							<div className="footer-stuff">
-								<a
-									className="aaa"
-									href="https://github.com/BenjaminKHLau"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									<img src={ben} className="footer-img" alt="github"/>
-								</a>
-								<a
-									className="aaa"
-									href="https://www.linkedin.com/in/benjaminkhlau/"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									<img src={linkedin} className="footer-img" alt="linkedin"/>
-								</a>
-							</div>
-								<div className="fname">Crafty is an Etsy inspired application by Benjamin Lau</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer>
+      <div className="Footer-container">
+        <div className="devs">
+          <div className="Meet">Meet The Developer!</div>
+          <div className="Footer">
+            <div className="Footer-inner">
+              <div className="footer-stuff">
+                <a
+                  className="aaa"
+                  href="https://github.com/BenjaminKHLau"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={ben} className="footer-img" alt="github" />
+                </a>
+                <a
+                  className="aaa"
+                  href="https://www.linkedin.com/in/benjaminkhlau/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={linkedin} className="footer-img" alt="linkedin" />
+                </a>
+              </div>
+              <div className="fname">
+                Crafty is an Etsy inspired application by Benjamin Lau
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
