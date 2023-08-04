3796ff890ff63b73649e6fcf4f884b55449860f0
Benjamin Lau <benjaminkhlau@gmail.com>
2023-08-04 14:23:39 -0500

    prettier format

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -9,46 +9,50 @@ import SignupFormModal from "./auth/SignupMODAL";
 import { useSelector } from "react-redux";
 
 const NavBar = () => {
-	const session = useSelector((state) => state.session);
-	const cart = useSelector(state => state.cart.cart)
-	const user = session.user ? session.user : null;
+  const session = useSelector((state) => state.session);
+  const cart = useSelector((state) => state.cart.cart);
+  const user = session.user ? session.user : null;
 
-	return (
-		<nav>
-			<Link to="/">
-				<img src={CraftyLogo} className="CraftyLogo" alt="Crafty's cool logo" />
-			</Link>
-			{!user && (
-				<div className="login-signup-container">
-					<div className="login-signup1">
-						<LoginFormModal />
-					</div>
+  return (
+    <nav>
+      <Link to="/">
+        <img src={CraftyLogo} className="CraftyLogo" alt="Crafty's cool logo" />
+      </Link>
+      {!user && (
+        <div className="login-signup-container">
+          <div className="login-signup1">
+            <LoginFormModal />
+          </div>
 
-					<div className="login-signup1">
-						<SignupFormModal />
-					</div>
-				</div>
-			)}
+          <div className="login-signup1">
+            <SignupFormModal />
+          </div>
+        </div>
+      )}
 
-			{user && (
-				<div className="logged-in-buttons">
-					<div className="login-signup-container">
-						<ShopFormModal />
-					</div>
-					<div className="login-signup-container">
-						<Link to="/profile" className="login-signup">Profile</Link>
-					</div>
-					<div className="login-signup-container">
-						<Link to="/cart" className="login-signup">Cart</Link>
-					<div className="cart-num">{cart.length}</div>
-					</div>
-					<div className="login-signup-container">
-						<LogoutButton />
-					</div>
-				</div>
-			)}
-		</nav>
-	);
+      {user && (
+        <div className="logged-in-buttons">
+          <div className="login-signup-container">
+            <ShopFormModal />
+          </div>
+          <div className="login-signup-container">
+            <Link to="/profile" className="login-signup">
+              Profile
+            </Link>
+          </div>
+          <div className="login-signup-container">
+            <Link to="/cart" className="login-signup">
+              Cart
+            </Link>
+            <div className="cart-num">{cart.length}</div>
+          </div>
+          <div className="login-signup-container">
+            <LogoutButton />
+          </div>
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default NavBar;
